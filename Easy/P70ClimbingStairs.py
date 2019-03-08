6e383ddb544842bed460672f30a324d99057c9de
Eric E Yu <eric.e.yu@oracle.com>
2019-03-08 11:16:04 +0800

    implemented easy P70

---
+++ Easy/P70ClimbingStairs.py
@@ -0,0 +1,100 @@
+"""
+Author    :   eeyu
+Date      :   2019/3/8 10:27
+File Name :   P70ClimbingStairs.py
+Description :   https://leetcode-cn.com/problems/climbing-stairs/
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Note: Given n will be a positive integer.
+
+Example 1:
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+===============================================================================
+                                     Change log
+===============================================================================
+
+    Date         GUID        Comment
+----------------------------------------------------------------
+    2019/3/8     eeyu   Initial creation
+    
+"""
+# coding: utf-8
+from functools import lru_cache
+
+class P70ClimbingStairs(object):
+
+    @staticmethod
+    @lru_cache(10)
+    def climb_stairs(n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        assert n > 0, 'stair number should be positive'
+
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            return P70ClimbingStairs.climb_stairs(n-1) + P70ClimbingStairs.climb_stairs(n-2)
+
+if __name__ == '__main__':
+    print(P70ClimbingStairs.climb_stairs(1))
+    print(P70ClimbingStairs.climb_stairs(2))
+    print(P70ClimbingStairs.climb_stairs(3))
+    print(P70ClimbingStairs.climb_stairs(4))
+    print(P70ClimbingStairs.climb_stairs(5))
+    print(P70ClimbingStairs.climb_stairs(6))
+    print(P70ClimbingStairs.climb_stairs(38))
+
+
+
+"""
+(1) 1
+1
+
+(2) 2
+1 -1 
+2
+
+(3)
+1 -1 -1
+1 -2 
+2 -1
+
+(4)
+1 - 1 - 1 - 1
+1 - 1  - 2
+1  - 2 - 1
+2 - 1 -1 
+2 - 2
+
+
+(5)
+1 - 1 - 1 - 1 - 1
+1 - 1 - 1 - 2
+1 - 2 -2 
+2 - 1 -1 - 1
+2 - 1 - 2
+2 - 2 - 1
+
+f(n) = f(n-1) + f(1)
+
+
+
+"""
