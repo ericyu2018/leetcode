c369d0e9641c5d842bf4bb4bdb8c0f1d502ca68d
Eric E Yu <eric.e.yu@oracle.com>
2019-03-18 14:01:09 +0800

    implemented easy P122

---
+++ Easy/P122BestTimeToBuyAndSellStockII.py
@@ -0,0 +1,66 @@
+"""
+Author    :   eeyu
+Date      :   2019/3/18 13:53
+File Name :   P122BestTimeToBuyAndSellStockII.py
+Description :   https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+Problem:
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and
+sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+Example 1:
+Input: [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+             Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+
+Example 2:
+Input: [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+             Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+             engaging multiple transactions at the same time. You must sell before buying again.
+
+Example 3:
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+===============================================================================
+                                     Change log
+===============================================================================
+
+    Date         GUID        Comment
+----------------------------------------------------------------
+    2019/3/18     eeyu   Initial creation
+    
+"""
+# coding: utf-8
+
+
+class P122BestTimeToBuyAndSellStockII(object):
+    def max_profit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+        if prices is None or len(prices) < 2:
+            return 0
+
+        max_profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
+
+        return max_profit
+
+
+if __name__ == '__main__':
+    print(P122BestTimeToBuyAndSellStockII().max_profit([7, 1, 5, 3, 6, 4]))
+    print(P122BestTimeToBuyAndSellStockII().max_profit([1, 2, 3, 4, 5]))
+    print(P122BestTimeToBuyAndSellStockII().max_profit([7, 6, 4, 3, 1]))
+
