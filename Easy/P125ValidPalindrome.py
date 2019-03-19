8aa28103d731ef60303a8689404b9c1af790cace
Eric E Yu <eric.e.yu@oracle.com>
2019-03-19 11:40:43 +0800

    added problem description for easy P125

--- Easy/P125ValidPalindrome.py
+++ Easy/P125ValidPalindrome.py
@@ -3,6 +3,18 @@ Author    :   eeyu
 Date      :   2019/3/19 9:56
 File Name :   P125ValidPalindrome.py
 Description :   https://leetcode-cn.com/problems/valid-palindrome/
+Problem:
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+Input: "A man, a plan, a canal: Panama"
+Output: true
+
+Example 2:
+Input: "race a car"
+Output: false
 ===============================================================================
                                      Change log
 ===============================================================================
