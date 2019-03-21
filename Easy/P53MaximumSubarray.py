e1380d1ae7ede72e7e9aca4166a0660b73ff7dcd
Eric E Yu <eric.e.yu@oracle.com>
2019-03-21 21:15:14 +0800

    implemented using dynamic programming

--- Easy/P53MaximumSubarray.py
+++ Easy/P53MaximumSubarray.py
@@ -95,5 +95,5 @@ if __name__ == '__main__':
    print(P53MaximumSubarray().maxSubArray([]))
    print(P53MaximumSubarray().maxSubArray([7]))
    print(P53MaximumSubarray().maxSubArray([1, -1, 1]))
-   print(P53MaximumSubarray().maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+   print(P53MaximumSubarray().maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
    print(P53MaximumSubarray().maxSubArray2([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
