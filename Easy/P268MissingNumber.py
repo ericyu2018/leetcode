220142bea7ea08e592cc36433bd6b5c4175e9e0b
Eric E Yu <eric.e.yu@oracle.com>
2019-04-08 10:50:59 +0800

    added problem information for easy P268

--- Easy/P268MissingNumber.py
+++ Easy/P268MissingNumber.py
@@ -3,6 +3,20 @@ Author    :   eeyu
 Date      :   2019/4/8 10:06
 File Name :   P268MissingNumber.py
 Description :  https://leetcode-cn.com/problems/missing-number/
+Problem:
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1:
+
+Input: [3,0,1]
+Output: 2
+Example 2:
+
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+
 ===============================================================================
                                      Change log
 ===============================================================================
